cfb0abb0e5f481c45030779aaedcf23acec2729b
Aïolah Vaïti <yoyo31.music@gmail.com>
2024-12-04 09:25:09 +0100

    Petite touche Aïolatesque

--- src/main/java/org/github/durand/Main.java
+++ src/main/java/org/github/durand/Main.java
@@ -4,5 +4,6 @@ public class Main {
     //commentaire
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Aïolah est passée par là 😎");
     }
 }
